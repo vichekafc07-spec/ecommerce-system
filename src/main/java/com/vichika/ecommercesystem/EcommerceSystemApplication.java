cc65e44c6bb9e3706abcddc3dbb5cb411ce72d62
Vichika <vichekafc07@gmail.com>
2026-05-16 15:09:50 +0700

    Ecommerce

---
+++ src/main/java/com/vichika/ecommercesystem/EcommerceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vichika.ecommercesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vichika/ecommercesystem/EcommerceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vichika.ecommercesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
