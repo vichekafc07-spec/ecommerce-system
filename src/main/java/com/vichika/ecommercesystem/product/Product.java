4e2b0968a9398349176a74033e1adbc5b7609aaf
Vichika <vichekafc07@gmail.com>
2026-05-16 22:12:54 +0700

    product feature

---
+++ src/main/java/com/vichika/ecommercesystem/product/Product.java
@@ -0,0 +1,23 @@
+package com.vichika.ecommercesystem.product;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String code;
+
+}
