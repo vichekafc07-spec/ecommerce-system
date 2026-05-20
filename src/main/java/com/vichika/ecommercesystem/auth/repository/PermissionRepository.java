ef5e48fba8585bf9ff46b82dc2b8de46e825baac
Vichika <vichekafc07@gmail.com>
2026-05-20 09:19:31 +0700

    admin feature

--- src/main/java/com/vichika/ecommercesystem/auth/repository/PermissionRepository.java
+++ src/main/java/com/vichika/ecommercesystem/auth/repository/PermissionRepository.java
@@ -4,4 +4,5 @@ import com.vichika.ecommercesystem.auth.model.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PermissionRepository extends JpaRepository<Permission,Integer> {
+    boolean existsByNameIgnoreCase(String name);
 }
