79023edff486b7bd8539a956d3d5547cc39a84aa
Vichika <vichekafc07@gmail.com>
2026-05-21 09:50:39 +0700

    auth feature

--- src/main/java/com/vichika/ecommercesystem/security/jwt/JwtService.java
+++ src/main/java/com/vichika/ecommercesystem/security/jwt/JwtService.java
@@ -1,63 +1,33 @@
 package com.vichika.ecommercesystem.security.jwt;
 
 import com.vichika.ecommercesystem.auth.model.AppUser;
-import com.vichika.ecommercesystem.auth.model.Permission;
 import com.vichika.ecommercesystem.auth.model.Role;
 import com.vichika.ecommercesystem.config.JwtConfig;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.Date;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class JwtService {
-
     private final JwtConfig jwtConfig;
 
-    public String generateAccessToken(AppUser user) {
-        return generateToken(user, jwtConfig.getAccessTokenExp());
+    public String generateAccessToken(AppUser user){
+        return generateToken(user,jwtConfig.getAccessTokenExp());
     }
 
-    public boolean isExpiration(String token) {
-        return getClaims(token).getExpiration().before(new Date());
-    }
-
-    public String getUsername(String token) {
-        return getClaims(token).getSubject();
-    }
-
-    public List<String> getRole(String token) {
-        return getClaims(token).get("role", List.class);
-    }
-
-    public List<String> getPermission(String token) {
-        return getClaims(token).get("permission", List.class);
-    }
-
-    private String generateToken(AppUser user, long tokenExpiration) {
-
+    public String generateToken(AppUser user, long tokenExpiration) {
         List<String> roles = user.getRoles()
                 .stream()
                 .map(Role::getName)
-                .toList();
-
-        List<String> permissions = user.getRoles()
-                .stream()
-                .flatMap(role -> role.getPermissions().stream())
-                .map(Permission::getName)
-                .distinct()
                 .toList();
 
         var claims = Jwts.claims()
                 .subject(user.getUsername())
                 .add("email", user.getEmail())
                 .add("role", roles)
-                .add("permission", permissions)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
                 .build();
@@ -66,13 +36,5 @@ public class JwtService {
                 .claims(claims)
                 .signWith(jwtConfig.getKey())
                 .compact();
-    }
-
-    private Claims getClaims(String token) {
-        return Jwts.parser()
-                .verifyWith(jwtConfig.getKey())
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
     }
 }
