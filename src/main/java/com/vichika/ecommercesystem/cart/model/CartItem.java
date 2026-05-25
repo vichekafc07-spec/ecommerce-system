6ee2301480101e63374e6450f0ce05e2a2bc6980
Vichika <vichekafc07@gmail.com>
2026-05-25 15:55:23 +0700

    cart feature

--- src/main/java/com/vichika/ecommercesystem/cart/model/CartItem.java
+++ src/main/java/com/vichika/ecommercesystem/cart/model/CartItem.java
@@ -18,13 +18,12 @@ public class CartItem {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "cart_id", nullable = false)
+    @JoinColumn(name = "cart_id")
     private Cart cart;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id", nullable = false)
+    @JoinColumn(name = "product_id")
     private Product product;
 
-    @Column(nullable = false)
     private Integer quantity;
 }
